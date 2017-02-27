6c2e5a2bc4197229b719c7f05a436fd2ba4e4e1d
Euge <angeleuge95@gmail.com>
2017-02-27 17:40:42 +0000

    Refactors count of students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 #first, we print the list of students
 puts "The students of Villains Academy"
 puts "-------------"
@@ -12,7 +13,5 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-#then we print the total count of students 
-print "Overall, we have "
-print 11
-print " great students."
+#then we print the total count of students
+puts "Overall, we have #{student_count} great students."
