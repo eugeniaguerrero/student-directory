8867bf36ae7710fcc9ae3a95539b1da3ff1f9f6b
Euge <angeleuge95@gmail.com>
2017-03-03 13:58:39 +0000

    Adds exercises for section 8

---
+++ 8-Exercises/directory_8-3.rb
@@ -0,0 +1,44 @@
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice."
+  #create an empty array
+  students = []
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
+
+# then we print the list of students
+def print(students)
+# loops through each student
+  students.each_with_index() do |student, index|
+# if the name begins with letter
+  if student[:name].length < 12 #prints if name is shorter than 12 characters
+    puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+# finally, we print the total count of students
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students."
+end
+
+students = input_students
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
